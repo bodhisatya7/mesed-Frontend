42c83f346512bc2b9407d54acaa767c7ee2f0dfb
bodhisatya7 <bodhisatya.banerjee@42hertz.com>
2019-06-04 11:22:50 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Route } from "react-router-dom";
-<<<<<<< HEAD
-import Login from './pages/login'
-import HomeScreen from './HomeScreen/HomeScreen'
-import Person from './Person/Person'
-import Artist from './Artist/Artist'
-import upload from './UploadImgToS3/UploadImgToS3'
-=======
 import Helmet from 'react-helmet';
 import Login from './pages/login';
 import Register from './pages/register';
@@ -18,7 +11,6 @@ import Fan from './pages/Fan/fanhome';
 import FanDash from './pages/Fan/fandash';
 import ArtistHome from './pages/Artist/artisthome';
 import ArtistDash from './pages/Artist/artistdash';
->>>>>>> 4a3186cc419d6b6ca144a97501f75a1d024a5229
 
 
 function App() {
@@ -32,14 +24,10 @@ function App() {
     <Route exact path="/register" component={Register} />
     <Route exact path="/artist" component={Artist} />
     <Route exact path="/person" component={Person} />
-<<<<<<< HEAD
-    <Route exact path="/upload" component={upload} />
-=======
     <Route exact path="/fan_home" component={Fan} />
     <Route exact path="/fan_dash" component={FanDash} />
     <Route exact path="/artist_home" component={ArtistHome} />
     <Route exact path="/artist_dash" component={ArtistDash} />
->>>>>>> 4a3186cc419d6b6ca144a97501f75a1d024a5229
     </div>);
   }
     
